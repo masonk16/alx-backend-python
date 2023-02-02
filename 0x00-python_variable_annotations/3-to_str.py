9da5701556887f97fd9d8b549f98ec7b53810ed5
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-02 12:42:35 +0000

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    :param n: float
+    :return: string representation of the float
+    """
+    return str(n)
