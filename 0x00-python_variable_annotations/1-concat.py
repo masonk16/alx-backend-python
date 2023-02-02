dc3138b2b9975722b73d6b65a324a23c244ad6cd
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-02 12:23:31 +0000

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ that takes a string str1 and a string str2 as arguments and returns a concatenated string
+    """
+    result = str1 + str2
+    return result
