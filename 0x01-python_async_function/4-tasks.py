ce2ce932f5f06fbae4b11a7205d3853f265d0bd0
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-06 11:28:30 +0000

    4. Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+4. Tasks
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Task_wait_random is being called.
+    """
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
